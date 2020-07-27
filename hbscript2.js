258ac864e5b1fc017990016f2c3d94e248f98746
baba0rum <allexandree@hotmail.fr>
2020-07-27 20:36:01 +0200

    Update hbscript2.js

--- hbscript2.js
+++ hbscript2.js
@@ -22,14 +22,6 @@ function updateAdmins() {
   room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
 }
 
-function updateAdmins() { 
-  // Get all players
-  var players = room.getPlayerList();
-  if ( players.length == 0 ) return; // No players left, do nothing.
-  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
-  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
-}
-
 room.onPlayerJoin = function(player) {
   updateAdmins();
   room.sendAnnouncement("Salut " + player.name + " !");
@@ -57,6 +49,8 @@ room.onPlayerTeamChange = function(changedPlayer, byPlayer) {
 
 setInterval(spectheafk, 1000);
 
+
+
 function spectheafk(){
 
 	var players = room.getPlayerList();
@@ -68,18 +62,16 @@ function spectheafk(){
 		  room.setPlayerTeam(players[i].id, 0);
 
 		}
-
 	}
-	
-	
 }
 
 
 
 
 room.onPlayerChat = function(player, message) {
- var res = message.split(" ", 2); 
+var res = message.split(" ", 8); 
  
+
 	if (res[0] == "afk")
 	{	
 		if (player.admin)
