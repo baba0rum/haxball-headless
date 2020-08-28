34ce3f169a53523d4642c380eb960d16f36730c6
baba0rum <allexandree@hotmail.fr>
2020-08-28 21:44:16 +0200

    Update hbscript2.js

--- hbscript2.js
+++ hbscript2.js
@@ -1,36 +1,43 @@
-window.onHBLoaded = () => {
-  const room = window.HBInit({
-	roomName: "18-25 JVC ISSOU",
-	maxPlayers: 30,
-	password: "1825",
-	public: true,
-	noPlayer: true
-});
-var afks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+window.onHBLoaded = () => {
+ const room = window.HBInit({
+roomName: "18-25 MARLOU-QLF AHI",
+maxPlayers: 30,
+password: "1825",
+public: true,
+noPlayer: true
+});
 
 room.setDefaultStadium("Big");
-room.setScoreLimit(5);
+room.setScoreLimit(3);
 room.setTimeLimit(0);
+
+var tireur = "";
 
 // If there are no admins left in the room give admin to one of the remaining players.
 function updateAdmins() { 
-  // Get all players
-  var players = room.getPlayerList();
-  if ( players.length == 0 ) return; // No players left, do nothing.
-  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
-  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
+ // Get all players
+ var players = room.getPlayerList();
+ if ( players.length == 0 ) return; // No players left, do nothing.
+ if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
+ room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
 }
 
 room.onPlayerJoin = function(player) {
-  updateAdmins();
-  room.sendAnnouncement("Salut " + player.name + " !");
-  afks[player.id] = 0;
+ updateAdmins();
+ room.sendAnnouncement("Salut " + player.name + " !");
+ playernumer = playernumer + 1;
+ 
+myTimer();
+isok = 0;
 }
 
 room.onPlayerLeave = function(player) {
-  updateAdmins();
-  afks[player.id] = 0;
+ updateAdmins();
+playernumer = playernumer -1;
+
+myTimer();
+isok = 0;
 
 }
 
@@ -38,137 +45,138 @@ room.onPlayerActivity = function(player) {
 
 }
 
+room.onPlayerBallKick = function(player) {
+
+tireur = player.name;
+}
+
+room.onTeamGoal = function(team) {
+
+if (tireur)
+{
+room.sendAnnouncement("Ayaaa " + tireur + " a marqué !");
+tireur = "";
+}
+}
+
 room.onPlayerTeamChange = function(changedPlayer, byPlayer) {
-	if (afks[changedPlayer.id] == 1)
-	{
-		changedPlayer.team = 0;
-		room.setPlayerTeam(changedPlayer.id, 0);
 
-	}
 }
-
-setInterval(spectheafk, 1000);
-
-
-
-function spectheafk(){
-
-	var players = room.getPlayerList();
-
-	for (i2 = 0; i2 < afks.length; i2++) {
-	if (afks[i2] == 1)
-		for (i = 0; i < players.length; i++) {
-		
-		  room.setPlayerTeam(players[i].id, 0);
-
-		}
-	}
-}
-
-
-
 
 room.onPlayerChat = function(player, message) {
+
 var res = message.split(" ", 8); 
  
 
-	if (res[0] == "afk")
-	{	
-		if (player.admin)
-		{
-			if (res[1] == "list")
-			{
-				var players2 = room.getPlayerList();
-				var playernames = "";
-				for (i2 = 0; i2 < players2.length; i2++)
-				{
-					playernames = playernames + players2[i2].name + " " + players2[i2].id.toString() + " ";
-				}
-				if (playernames.lenght > 100)
-				{
-					room.sendAnnouncement(playernames.slice(0, 100), player.id);
-					room.sendAnnouncement(playernames.slice(100, playernames.lenght), player.id);
-				}
-				else
-				{
-					room.sendAnnouncement(playernames, player.id);
-
-				}	
-				return false;					
-
-			}	
-			var players = room.getPlayerList();
-			for (i = 0; i < players.length; i++)
-			{
-				if (res[1] == players[i].name)
-				{
-					if (afks[i] == 0)
-					{
-						afks[i] = 1;
-						room.sendAnnouncement(players[i].name + " est maintenant afk");
-						return false;	
-
-					}
-					else
-					{
-						afks[i] = 0;
-						room.sendAnnouncement(players[i].name + " n'est plus afk");
-						return false;	
-					}
-				}
-				else if (res[1] == players[i].id)
-				{
-					if (afks[i] == 0)
-						{
-							afks[i] = 1;
-							room.sendAnnouncement(players[i].name + " est maintenant afk");
-							return false;	
-
-						}
-						else
-						{
-							afks[i] = 0;
-							room.sendAnnouncement(players[i].name + " n'est plus afk");
-							return false;	
-						}
-				}
-				
-				
-			}
-			
-			
-		
-		}
-		else
-		{
-			if (afks[player.id] == 1)
-			{
-				afks[player.id] = 0;
-				room.sendAnnouncement(player.name + " n'est plus afk");
-				return false;
-
-			}
-			else 
-			{
-				afks[player.id] = 1;
-				room.sendAnnouncement(player.name + " est maintenant afk");
-				return false;
-			}
-		
-		}
-	}
-    if (message == "adminstp")
-	{
-		room.setPlayerAdmin(player.id, true);
-		return false;
-	}
-	if (message == "adminremove")
-	{
-		room.setPlayerAdmin(player.id, false);
-		return false;
-	}
-	
+ if (message == "adminstp")
+{
+room.setPlayerAdmin(player.id, true);
+return false;
 }
+if (message == "adminremove")
+{
+room.setPlayerAdmin(player.id, false);
+return false;
+}
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("generalchat").value += player.name + ": " + message + '\r\n';
+}
+
+
+var myvar2 = setInterval(resetgeneralchat, 300000);
+
+var add_input = function () {
+
+ var count = 0;
+
+ return function add_input() {
+ count++;
+ if (count >= 10) {
+ return false;
+ }
+ var input = document.createElement('input');
+ input.name = 'generated_input';
+input.id = 'schatinput';
+ document.getElementsByTagName("iframe")[0].contentDocument.getElementById("schat").appendChild(input);
+
+
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("schatinput").addEventListener('keypress', function(e) {
+if (e.keyCode == 13) {
+room.sendAnnouncement("Admin: " + document.getElementsByTagName("iframe")[0].contentDocument.getElementById("schatinput").value);
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("schatinput").value = "";
+ }
+ });
+ }
+
+}();
+
+var myVar = setInterval(myTimer, 1000);
+var isok = 0;
+function myTimer() {
+
+if (isok == 0)
+{
+if (document.getElementsByTagName("iframe")[0])
+{
+isok = 1;
+if (!document.getElementsByTagName("iframe")[0].contentDocument.getElementById("playernum"))
+{
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("roomlink").innerHTML = document.getElementsByTagName("iframe")[0].contentDocument.getElementById("roomlink").innerHTML + '<div id="playernum" class="plnm">Players: </div> <br> <div id="schat"></div>'
+if (document.getElementsByName("generated_input"))
+{
+add_input();
+addtextarea();
+
+}
+}
+}
+if (document.getElementsByTagName("iframe")[0].contentDocument.getElementById("playernum").innerHTML)
+{
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("playernum").innerHTML = "Players: " + playernumer + '<br> <div id="schat"></div>';
+isok = 2;
+if (!document.getElementsByName("generated_input"))
+{
+add_input();
+addtextarea();
+}
+
+}
+}
+} 
+
+
+function resetgeneralchat() {
+
+document.getElementsByTagName("iframe")[0].contentDocument.getElementById("generalchat").value = "";
+
+} 
+
+var playernumer =0;
+
+
+
+
+var addtextarea = function () {
+
+ var count = 0;
+
+ return function addtextarea() {
+ count++;
+ if (count >= 10) {
+ return false;
+ }
+ var input = document.createElement("TEXTAREA");
+ input.name = 'generated_chat';
+input.id = 'generalchat';
+input.rows = 15;
+input.cols = 60;
+ document.getElementsByTagName("iframe")[0].contentDocument.getElementById("schat").appendChild(input);
+
+}
+
+}();
+
+
+
 
 
 }
@@ -177,3 +185,4 @@ if (typeof window.HBInit === 'function')
 {
 window.onHBLoaded()
 }
+
